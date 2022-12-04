34116dbabe545b41bcf46e6d559041622dc311a7
SainathKamble <sainath.kamble@vnnogile.in>
2022-12-04 22:37:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,102 @@
 import './App.css';
 import FlashcardList from './FlashcardList';
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  const [flashcards, setFlashcards] = useState(FLASHCARD_LIST)
-  return (
-    <FlashcardList flashcards = {flashcards} ></FlashcardList>
-  );
+    const [flashcards, setFlashcards] = useState(FLASHCARD_LIST)
+    const [categories, setCategories] = useState([])
+    const categoryEl = useRef();
+    const amountRef = useRef();
+    useEffect(()=>{
+        axios.get("https://opentdb.com/api_category.php").then((res)=>{
+            setCategories(res.data.trivia_categories)
+        })
+    },[])
+    useEffect(() => {
+       
+    }, [])
+
+    function handleSubmit(e){
+        e.preventDefault()
+        axios.get("https://opentdb.com/api.php",{
+            params : {
+                amount : amountRef.current.value,
+                category : categoryEl.current.value
+            }
+        }).then((res) => {
+            console.log(res.data)
+            setFlashcards(res.data.results.map((questionItem, index) => {
+                const answer = questionItem.correct_answer
+                const options = [
+                    ...questionItem.incorrect_answers.map(a => decodeString(a)),
+                    decodeString(answer)
+                ]
+                return {
+                    id: `${index}-${
+                        Date.now()
+                    }`,
+                    question: decodeString(questionItem.question),
+                    answer: decodeString(answer),
+                    options: options.sort(() => Math.random() - 0.5)
+
+                }
+            }))
+        })
+    }
+
+    return (
+        <>
+            <form className='header' onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <label htmlFor='category'>Category</label>
+                    <select id='category' ref={categoryEl}>
+                        {categories.map((category)=>{
+                            return <option value={category.id} key={category.name}>{category.name}</option>
+                        })}
+                    </select>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='amount'>Number of questions</label>
+                    <input type='number' id='amount' min='1' step="1" defaultValue={10} ref={amountRef}/>
+                </div>
+                <div className='form-group btn'>
+                    <button>Generate</button>
+                </div>
+            </form>
+            <div className='container'>
+                <FlashcardList flashcards={flashcards}></FlashcardList>
+            </div>
+        </>
+
+
+    );
+}
+
+function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value
 }
 
 
-
-const FLASHCARD_LIST = 
-[
+const FLASHCARD_LIST = [
     {
         "id": 1,
-        "category": "Art",
+        "category": "Legendary",
         "type": "multiple",
         "difficulty": "medium",
-        "question": "Which time signature is commonly known as &ldquo;Cut Time?&rdquo;",
-        "answer": "2/2",
-        "options": [
-            "4/4",
-            "6/8",
-            "3/4",
-            "2/2"
-        ]
-    },
-    {
-        "id" : 2, 
-        "category": "Science: Computers",
+        "question": "What is my name?",
+        "answer": "Sainath",
+        "options": ["psynath", "ksainath", "cainath", "Sainath"]
+    }, {
+        "id": 2,
+        "category": "Lengendary",
         "type": "multiple",
         "difficulty": "medium",
-        "question": "Which internet company began life as an online bookstore called &#039;Cadabra&#039;?",
-        "answer": "Amazon",
-        "options": [
-            "eBay",
-            "Overstock",
-            "Shopify",
-            "Amazon"
-        ]
+        "question": "Favorite athelete?",
+        "answer": "Khabib Nurmagomedov",
+        "options": ["Khabib Nurmagomedov", "LeBron James", "Virat Kohli", "Cristiano Ronaldo"]
     },
 ]
 
--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,23 +1,39 @@
-import React, {useState} from "react"
+import React, {useEffect, useRef, useState} from "react"
 
 export default function Flashcard( {flashcard} ) {
   const [flip, setFlip] = useState(false)
+  const [height, setHeight] = useState("initial");
+
+  const frontEl = useRef()
+  const backEl = useRef()
+
+  function setMaxHeight(){
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backHeight = backEl.current.getBoundingClientRect().height
+    setHeight(Math.max(frontHeight, backHeight, 100));
+  }
+
+  useEffect(setMaxHeight,[flashcard.question, flashcard.options, flashcard.answer])
+  useEffect(()=>{
+    window.addEventListener("resize",setMaxHeight);
+    return window.removeEventListener("resize", setMaxHeight);
+  })
+
   return (
-    <div onClick={()=> setFlip(!flip)} className={`card ${flip ? "flip" : ""}`}>
-    <div className="front">
+    <div onClick={()=> setFlip(!flip)} className={`card ${flip ? 'flip' : ''}`} style={{height : height}} >
+    <div className="front" ref={frontEl}>
       {flashcard.question}
       <div className="flashcard-options">
         {flashcard.options.map((option)=>{
-          return <div className="flashcard-options">
+          return <div className="flashcard-option" key={option}>
             {option}
             </div>
         })}
       </div>
     </div>
-    <div className="back">
+    <div className="back" ref={backEl}>
       {flashcard.answer}
     </div>
     </div>
   )
 }
-
